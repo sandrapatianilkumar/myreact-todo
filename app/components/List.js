34634180d8af2d56c09706254a3e6fa53c2791a1
sandrapatianilkumar <sandrapati.anilkumar@gmail.com>
2017-09-24 17:11:30 +0530

    added ListItem component to manage each check box state

--- app/components/List.js
+++ app/components/List.js
@@ -1,32 +1,21 @@
 import React, {Component} from 'react';
+import ListItem from './ListItem';
 
 class List extends Component {
-    constructor(props){
-        super(props);
-        this.state={done:false}
-    }
-
-    handleOnChange(){
-        let done=!this.state.done;
-        this.setState({done });
-    }
-
+    
     render(){
         return(
                 <ul>
                     { 
-                        this.props.source.map((item,index) => <li key={index}>
-                            <input 
-                            checked={this.state.done}
-                            onChange={this.handleOnChange.bind(this)}
-                            type="checkbox" style={{fontSize:'x-large'}}/>
-                            {item} 
-                            <a href="#" onClick={this.props.handleDelete.bind(null,item)}>
-                                [X]
-                                </a>
-                            </li>)
+                        this.props.source.map((item,index) =>{
+                            return <ListItem
+                             key={item}
+                             handleDelete={this.props.handleDelete.bind(null,item)}
+                             item={item} index={index} />
+                        } )
                     }
                 </ul>
+                
             )
     }
 }
