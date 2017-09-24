e80f947252facd5f195ed6675419ef7d0e03d25e
sandrapatianilkumar <sandrapati.anilkumar@gmail.com>
2017-09-24 16:33:28 +0530

    modified list function to component and added complete basic function

--- app/components/List.js
+++ app/components/List.js
@@ -1,13 +1,33 @@
-import React from 'react';
-const List= props =>(
-    <ul>
-        { 
-            props.source.map((item,index) => <li key={index}>
-                {item} <a href="#" onClick={props.handleDelete.bind(null,item)}>
-                    [X]
-                    </a>
-                </li>)
-        }
-    </ul>
-)
+import React, {Component} from 'react';
+
+class List extends Component {
+    constructor(props){
+        super(props);
+        this.state={done:false}
+    }
+
+    handleOnChange(){
+        let done=!this.state.done;
+        this.setState({done });
+    }
+
+    render(){
+        return(
+                <ul>
+                    { 
+                        this.props.source.map((item,index) => <li key={index}>
+                            <input 
+                            checked={this.state.done}
+                            onChange={this.handleOnChange.bind(this)}
+                            type="checkbox" style={{fontSize:'x-large'}}/>
+                            {item} 
+                            <a href="#" onClick={this.props.handleDelete.bind(null,item)}>
+                                [X]
+                                </a>
+                            </li>)
+                    }
+                </ul>
+            )
+    }
+}
 export default List; 
